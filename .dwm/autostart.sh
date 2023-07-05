a65b9408bc4407758a90c5a9f5eff1d11c0a9042
liquidsworrds <adithyasunil@pm.me>
2023-07-05 15:22:40 +0530

    1

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+source "$HOME/.zshenv"
 nm-applet&
 dunst&
 sxhkd&
 picom&
 redshift -P -O 5500
 feh --bg-fill $WALL
+xinput set-prop 13 314 1
 dwmblocks&
---
+++ .zshenv
@@ -0,0 +1,5 @@
+fpath+=$HOME/.zsh/typewritten
+LANG=en_US.UTF-8
+TYPEWRITTEN_CURSOR="block"
+WALL="$HOME/pics/walls/spring.jpg"
+
